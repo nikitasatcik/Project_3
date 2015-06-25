bb9544d88caee45e6f247252f2a6d60146413c32
nikitasatcik <nikitasatcik2008@gmail.com>
2015-06-25 13:26:29 +0300

    Update WeaponFactory.java

--- src/main/java/WeaponFactory.java
+++ src/main/java/WeaponFactory.java
@@ -5,19 +5,26 @@ public class WeaponFactory {
         SAX, STAX, DOM
     }
 
-    public void createWeapon(String parser)  {
+    public void createWeapon(String parser) {
         ParsersType type = ParsersType.valueOf(parser.toUpperCase());
+        XMLValidator validator = new XMLValidator();
         switch (type) {
             case SAX:
+                validator.validate(XMLTags.XSD_PATH, XMLTags.XML_PATH);
                 new ReadXMLWithSAX().readXML(XMLTags.XML_PATH);
+                new XMLConvert().convertXML(XMLTags.XML_PATH, XMLTags.HTML_PATH, XMLTags.XSL_PATH);
                 break;
 
             case DOM:
+                validator.validate(XMLTags.XSD_PATH, XMLTags.XML_PATH);
                 new ReadXMLWithDom().readXML(XMLTags.XML_PATH);
+                new XMLConvert().convertXML(XMLTags.XML_PATH, XMLTags.HTML_PATH, XMLTags.XSL_PATH);
                 break;
 
             case STAX:
+                validator.validate(XMLTags.XSD_PATH, XMLTags.XML_PATH);
                 new ReadXMLWithStAX().readXML(XMLTags.XML_PATH);
+                new XMLConvert().convertXML(XMLTags.XML_PATH, XMLTags.HTML_PATH, XMLTags.XSL_PATH);
                 break;
 
             default:
