110bca1d60a0683400918b460732bfbdc8143f95
nikitasatcik <nikitasatcik2008@gmail.com>
2015-06-25 13:18:20 +0300

    Update ReadXMLWithDom.java

--- src/main/java/ReadXMLWithDom.java
+++ src/main/java/ReadXMLWithDom.java
@@ -1,22 +1,25 @@
 import constants.XMLTags;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.Namespace;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ReadXMLWithDom {
     private List<Weapon> weaponList = new ArrayList<>();
 
-    public  void readXML(String filePath) {
+    public void readXML(String filePath) {
         SAXBuilder builder = new SAXBuilder();
         try {
+            Namespace namespace = Namespace.getNamespace("myspace", "http://www.weapon.com/Weapon");
             Document document = builder.build(filePath);
             Element rootNode = document.getRootElement();
-            List list = rootNode.getChildren(XMLTags.GUN);
+            List list = rootNode.getChildren(XMLTags.GUN, namespace);
 
             System.out.println("Dom parsing started...");
 
@@ -24,17 +27,25 @@ public class ReadXMLWithDom {
                 Element node = (Element) aList;
                 Weapon weapon = new Weapon();
                 weapon.setModelName(node.getAttributeValue(XMLTags.MODEL_NAME));
-                weapon.setSerialModel(node.getChild("model").getAttributeValue(XMLTags.SERIAL_MODEL));
-                weapon.setOrigin(node.getChildText(XMLTags.ORIGIN));
-                weapon.setHandy(Integer.parseInt(node.getChildText(XMLTags.HANDY)));
-                weapon.ttc.setDeadRange(Integer.parseInt(node.getChild(XMLTags.TTC).getChild(XMLTags.RANGE).getAttributeValue(XMLTags.DEAD_RANGE)));
-                weapon.ttc.setMaxRange(Integer.parseInt(node.getChild(XMLTags.TTC).getChild(XMLTags.RANGE).getAttributeValue(XMLTags.MAX_RANGE)));
-                weapon.ttc.setMagazine(Boolean.parseBoolean(node.getChild("ttc").getChildText(XMLTags.MAGAZINE)));
-                weapon.ttc.setOptic(Boolean.parseBoolean(node.getChild("ttc").getChildText(XMLTags.OPTIC)));
+                weapon.setSerialModel(node.getChild(XMLTags.MODEL, namespace).getAttributeValue(XMLTags.SERIAL_MODEL));
+                weapon.setOrigin(node.getChildText(XMLTags.ORIGIN, namespace));
+                weapon.setHandy(Integer.parseInt(node.getChildText(XMLTags.HANDY, namespace)));
+
+                weapon.ttc.setDeadRange(Integer.parseInt(node.getChild(XMLTags.TTC, namespace).
+                        getChild(XMLTags.RANGE, namespace).getAttributeValue(XMLTags.DEAD_RANGE)));
+
+                weapon.ttc.setMaxRange(Integer.parseInt(node.getChild(XMLTags.TTC, namespace).
+                        getChild(XMLTags.RANGE, namespace).getAttributeValue(XMLTags.MAX_RANGE)));
+
+                weapon.ttc.setMagazine(Boolean.parseBoolean(node.getChild(XMLTags.TTC, namespace).
+                        getChildText(XMLTags.MAGAZINE, namespace)));
+
+                weapon.ttc.setOptic(Boolean.parseBoolean(node.getChild(XMLTags.TTC, namespace).
+                        getChildText(XMLTags.OPTIC, namespace)));
+
                 weaponList.add(weapon);
             }
-
-            System.out.println("Amount of weapon: " + weaponList.size());
+            Collections.sort(weaponList, (o1, o2) -> o1.getOrigin().compareTo(o2.getOrigin()));
             weaponList.forEach(System.out::println);
 
         } catch (IOException | JDOMException e) {
